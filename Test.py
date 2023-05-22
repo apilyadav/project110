e05a3f2dcdc8daa6c1831c6a9e79fa5655f6ba5e
apilyadav <103313280+apilyadav@users.noreply.github.com>
2023-05-22 14:43:51 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,33 @@
+import random
+no= random.randint(1,6)
+if no==1:
+  print("[----]")
+  print("[    ]")
+  print("[  0 ]")
+  print("[----]")
+elif no==2:
+      print("[----]")    
+      print("[    ]")     
+      print("[0 0]") 
+      print("[----]")
+elif no==3:
+      print("[----]")    
+      print("[    ]")     
+      print("[0 0 0]") 
+      print("[----]")      
+elif no==4:
+      print("[----]")    
+      print("[0 0]")     
+      print("[0 0]") 
+      print("[----]")  
+elif no==5:
+      print("[----]")    
+      print("[0 0]")     
+      print("[ 0 ]") 
+      print("[0 0]") 
+      print("[----]")      
+elif no==6:
+      print("[----]")    
+      print("[0 0 0]")     
+      print("[0 0 0]") 
+      print("[----]")          
